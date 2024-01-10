03a438eb21b633e1c3270c045c995359917e612e
juacrumar <juacrumar@lairen.eu>
2024-01-10 17:00:44 +0100

    update makefile and build

--- build.py
+++ build.py
@@ -1,8 +1,16 @@
 # Build script ran by poetry to compile the cuda kernels before installation
 from pathlib import Path
+import shutil
 import subprocess as sp
 
+import tensorflow as tf
+
 CUDA_FOLDER = Path("bgtrees") / "finite_gpufields" / "cuda_operators"
+
+
+def fake_cuda(op_name):
+    """Create an empty file to avoid the compilation of the cuda kernel"""
+    (CUDA_FOLDER / f"{op_name}.cuo").write_text("")
 
 
 def cuda_compile():
@@ -11,10 +19,23 @@ def cuda_compile():
     """
     # TODO: make them fail loudly (check=True)
     # TODO: make them fallback to CPU-only compilation instead of loudly failing
+    kerdef = ""
+    ops_to_compile = ["dot_product"]
+
+    # Check whether nvcc is available
+    nvcc_available = shutil.which("nvcc") is not None
+    tf_cuda = tf.test.is_gpu_available()
+    if nvcc_available and tf_cuda:
+        kerdef = "KERNEL_DEF='-D GOOGLE_CUDA=1'"
+    else:
+        kerdef = "KERNEL_DEF=''"
+        for op_name in ops_to_compile:
+            fake_cuda(op_name)
+
     # clean the directory
     sp.run(["make", "clean"], cwd=CUDA_FOLDER, shell=True)
-    # compile dot_product
-    sp.run(["make", "dot_product.so"], cwd=CUDA_FOLDER, shell=True)
+    for op_name in ops_to_compile:
+        sp.run(["make", f"{op_name}.so", kerdef], cwd=CUDA_FOLDER, shell=True)
 
 
 if __name__ == "__main__":
