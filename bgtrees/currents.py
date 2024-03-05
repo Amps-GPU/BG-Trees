e3aab7c50f1e844afc47bcc0d8f7a5144106b7c9
GDeLaurentis <g.dl@hotmail.it>
2024-03-05 23:21:45 +0000

    Added test for 6-point MHV tree; reworked recursion to have lmoms and lpols as global variables, on which sliced views are taken from two integer input (for now, due to cyclicity); this allowed to implement lru caching, which gives a factor of 2 speed up for the 6-point MHV tree.

--- bgtrees/currents.py
+++ bgtrees/currents.py
@@ -1,39 +1,46 @@
 from .metric_and_verticies import η, V3g, V4g
 
+import functools
 import numpy
 
 
-# @functools.lru_cache
 # @gpu_function
 def J_μ(lmoms, lpols, put_propagator=True, depth=0, verbose=False, einsum=numpy.einsum):
     """Recursive vectorized current builder. End of recursion is polarization tensors."""
-    if verbose:
-        print(f"Calling currents: {depth} - {lmoms.shape}")
+
     assert lmoms.shape[:2] == lpols.shape[:2]
     replicas, multiplicity, D = lmoms.shape
     Ds = lpols.shape[-1]
     assert D == Ds
-    if multiplicity == 1:
-        return einsum("mn,rn->rm", η(D), lpols[:, 0])
-    else:
-        if put_propagator:
-            tot_moms = einsum("rid->rd", lmoms)
-            propagators = 1 / einsum("rm,mn,rn->r", tot_moms, η(D), tot_moms)
-        Jrμ = (sum([einsum("rmno,rn,ro->rm",
-                           V3g(einsum("rim->rm", lmoms[:, :i, :]),
-                               einsum("rim->rm", lmoms[:, i:, :])),
-                           J_μ(lmoms[:, :i], lpols[:, :i], depth=depth + 1, verbose=verbose),
-                           J_μ(lmoms[:, i:], lpols[:, i:], depth=depth + 1, verbose=verbose)
-                           ) for i in range(1, multiplicity)]) +
-               sum([einsum("mnop,rn,ro,rp->rm",
-                           V4g(D),
-                           J_μ(lmoms[:, :i], lpols[:, :i], depth=depth + 1, verbose=verbose),
-                           J_μ(lmoms[:, i:j], lpols[:, i:j], depth=depth + 1, verbose=verbose),
-                           J_μ(lmoms[:, j:], lpols[:, j:], depth=depth + 1, verbose=verbose))
-                    for i in range(1, multiplicity - 1) for j in range(i + 1, multiplicity)])
-               )
-        Jr_μ = einsum("mn,rn->rm", η(D), Jrμ)
-        return einsum("r,rm->rm", propagators, Jr_μ) if put_propagator else Jr_μ
+
+    @functools.lru_cache
+    def _J_μ(a, b, put_propagator=True, ):
+        if verbose:
+            print(f"Calling currents: a = {a}, b = {b}.")
+        multiplicity = b - a
+        if multiplicity == 1:
+            return einsum("mn,rn->rm", η(D), lpols[:, a])
+        else:
+            if put_propagator:
+                tot_moms = einsum("rid->rd", lmoms[:, a:b])
+                propagators = 1 / einsum("rm,mn,rn->r", tot_moms, η(D), tot_moms)
+            Jrμ = (sum([einsum("rmno,rn,ro->rm",
+                               V3g(einsum("rim->rm", lmoms[:, a:a + i, :]),
+                                   einsum("rim->rm", lmoms[:, a + i:b, :])),
+                               _J_μ(a, a + i, ),
+                               _J_μ(a + i, b, )
+                               ) for i in range(1, multiplicity)]) +
+                   sum([einsum("mnop,rn,ro,rp->rm",
+                               V4g(D),
+                               _J_μ(a, a + i, ),
+                               _J_μ(a + i, a + j, ),
+                               _J_μ(a + j, b, )
+                               ) for i in range(1, multiplicity - 1) for j in range(i + 1, multiplicity)])
+                   )
+            Jr_μ = einsum("mn,rn->rm", η(D), Jrμ)
+            return einsum("r,rm->rm", propagators, Jr_μ) if put_propagator else Jr_μ
+
+    return _J_μ(0, multiplicity, put_propagator=put_propagator, )
 
 
 def forest(lmoms, lpols, verbose=False, einsum=numpy.einsum):
--- tests/test_currents.py
+++ tests/test_currents.py
@@ -46,3 +46,38 @@ def test_ward_identity():
         "rm,rm->r", lmoms[:, 0],
         J_μ(lmoms[:, 1:], lpols[:, 1:], put_propagator=False, verbose=True, )
     ) == 0)
+
+
+def test_MHV_amplitude_in_D_eq_4():
+    chosen_field = Field("finite field", 2 ** 31 - 19, 1)
+    lips.spinor_convention = "asymmetric"
+
+    helconf = "ppmmmm"
+    lPs = [Particles(len(helconf), field=chosen_field, seed=i) for i in range(25)]
+    for oPs in lPs:
+        oPs.helconf = helconf
+
+    lmoms = numpy.array([numpy.array([numpy.block([oParticle.four_mom, numpy.array([0, 0, 0, 0])])
+                                      for oParticle in oParticles]) for oParticles in lPs])
+    print(lmoms.shape)  # e.g.: (25 replicas, 6 gluon momenta, 8 dimensions)
+
+    lpols = numpy.array([numpy.array([numpy.block([εp(oParticles, index + 1), numpy.array([0, 0, 0, 0])])
+                                      if oParticles.helconf[index] == "p" else
+                                      numpy.block([εm(oParticles, index + 1), numpy.array([0, 0, 0, 0])])
+                                      if oParticles.helconf[index] == "m" else
+                                      numpy.block([numpy.array([0, 0, 0, 0, 0, 0, 1, 0])])
+                                      if oParticles.helconf[index] == "x" else
+                                      None for index in range(len(helconf))]) for oParticles in lPs])
+    print(lpols.shape)
+
+    D = lmoms.shape[-1]
+
+    # momentum is conserved
+    assert numpy.all(numpy.einsum("rim->rm", lmoms) == 0)
+    # polarization . momentum is zero
+    assert numpy.all(numpy.einsum("rim,mn,rin->ri", lmoms, η(D), lpols) == 0)
+    # polarization . current is zero
+    assert numpy.all(numpy.einsum(
+        "rm,rm->r", lpols[:, 0],
+        J_μ(lmoms[:, 1:], lpols[:, 1:], put_propagator=False, verbose=True, )
+    ) == numpy.array([oPs("(32[12]^4)/([12][23][34][45][56][61])") for oPs in lPs]))
