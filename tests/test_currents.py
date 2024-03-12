a2d17028fca2cffb40e09dacefb6cd90db4f5e7d
juacrumar <juacrumar@lairen.eu>
2024-03-12 14:36:51 +0100

    separate the generation of momentum and polarization to reuse it

--- tests/test_currents.py
+++ tests/test_currents.py
@@ -1,39 +1,62 @@
-import numpy
 import lips
-
 from lips import Particles
+import numpy
 from syngular import Field
 
-from bgtrees.metric_and_verticies import η
-from bgtrees.states import εp, εm
 from bgtrees.currents import J_μ
+from bgtrees.metric_and_verticies import η
+from bgtrees.states import εm, εp
+
+lips.spinor_convention = "asymmetric"
 
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+def _generate_input(chosen_field, helconf, n=25):
+    """Generate the momentum and polarization arrays using lips
+    Returns the momentum and polarization as numpy array and the
+    list of lips particle_lists
+    """
+    lmoms = []
+    lpols = []
+    lPs = []
+
+    for seed in range(n):
+        particle_list = Particles(len(helconf), field=chosen_field, seed=seed)
+        particle_list.helconf = helconf
+
+        # Prepare hte momentum array
+        lm = []
+        for oParticle in particle_list:
+            lm.append(numpy.block([oParticle.four_mom, numpy.array([0, 0, 0, 0])]))
+        lmoms.append(numpy.array(lm))
+
+        # Prepare the polarization array
+        lp = []
+        for index, helconf_index in enumerate(helconf):
+            if helconf_index == "p":
+                tmp = [εp(particle_list, index + 1), numpy.array([0, 0, 0, 0])]
+            elif helconf_index == "m":
+                tmp = [εm(particle_list, index + 1), numpy.array([0, 0, 0, 0])]
+            elif helconf_index == "x":
+                tmp = [numpy.array([0, 0, 0, 0, 0, 0, 1, 0])]
+            else:
+                tmp = None
+            lp.append(numpy.block(tmp))
+
+        lpols.append(numpy.array(lp))
+        lPs.append(particle_list)
+
+    lmoms = numpy.array(lmoms)
+    lpols = numpy.array(lpols)
+
+    return lmoms, lpols, lPs
 
 
 def test_ward_identity():
-    chosen_field = Field("finite field", 2 ** 31 - 19, 1)
+    chosen_field = Field("finite field", 2**31 - 19, 1)
     # chosen_field = Field("mpc", 0, 32)
-    lips.spinor_convention = "asymmetric"
-
     helconf = "ppmmmm"
-    lPs = [Particles(len(helconf), field=chosen_field, seed=i) for i in range(25)]
-    for oPs in lPs:
-        oPs.helconf = helconf
 
-    lmoms = numpy.array([numpy.array([numpy.block([oParticle.four_mom, numpy.array([0, 0, 0, 0])])
-                                      for oParticle in oParticles]) for oParticles in lPs])
-    print(lmoms.shape)  # e.g.: (25 replicas, 6 gluon momenta, 8 dimensions)
-
-    lpols = numpy.array([numpy.array([numpy.block([εp(oParticles, index + 1), numpy.array([0, 0, 0, 0])])
-                                      if oParticles.helconf[index] == "p" else
-                                      numpy.block([εm(oParticles, index + 1), numpy.array([0, 0, 0, 0])])
-                                      if oParticles.helconf[index] == "m" else
-                                      numpy.block([numpy.array([0, 0, 0, 0, 0, 0, 1, 0])])
-                                      if oParticles.helconf[index] == "x" else
-                                      None for index in range(len(helconf))]) for oParticles in lPs])
-    print(lpols.shape)
+    lmoms, lpols, _ = _generate_input(chosen_field, helconf, 25)
 
     D = lmoms.shape[-1]
 
@@ -42,33 +65,21 @@ def test_ward_identity():
     # polarization . momentum is zero
     assert numpy.all(numpy.einsum("rim,mn,rin->ri", lmoms, η(D), lpols) == 0)
     # polarization . current is zero
-    assert numpy.all(numpy.einsum(
-        "rm,rm->r", lmoms[:, 0],
-        J_μ(lmoms[:, 1:], lpols[:, 1:], put_propagator=False, verbose=True, )
-    ) == 0)
+    assert numpy.all(
+        numpy.einsum(
+            "rm,rm->r",
+            lmoms[:, 0],
+            J_μ(lmoms[:, 1:], lpols[:, 1:], put_propagator=False, verbose=True),
+        )
+        == 0
+    )
 
 
 def test_MHV_amplitude_in_D_eq_4():
-    chosen_field = Field("finite field", 2 ** 31 - 19, 1)
-    lips.spinor_convention = "asymmetric"
-
+    chosen_field = Field("finite field", 2**31 - 19, 1)
     helconf = "ppmmmm"
-    lPs = [Particles(len(helconf), field=chosen_field, seed=i) for i in range(25)]
-    for oPs in lPs:
-        oPs.helconf = helconf
 
-    lmoms = numpy.array([numpy.array([numpy.block([oParticle.four_mom, numpy.array([0, 0, 0, 0])])
-                                      for oParticle in oParticles]) for oParticles in lPs])
-    print(lmoms.shape)  # e.g.: (25 replicas, 6 gluon momenta, 8 dimensions)
-
-    lpols = numpy.array([numpy.array([numpy.block([εp(oParticles, index + 1), numpy.array([0, 0, 0, 0])])
-                                      if oParticles.helconf[index] == "p" else
-                                      numpy.block([εm(oParticles, index + 1), numpy.array([0, 0, 0, 0])])
-                                      if oParticles.helconf[index] == "m" else
-                                      numpy.block([numpy.array([0, 0, 0, 0, 0, 0, 1, 0])])
-                                      if oParticles.helconf[index] == "x" else
-                                      None for index in range(len(helconf))]) for oParticles in lPs])
-    print(lpols.shape)
+    lmoms, lpols, lPs = _generate_input(chosen_field, helconf, 25)
 
     D = lmoms.shape[-1]
 
@@ -77,7 +88,11 @@ def test_MHV_amplitude_in_D_eq_4():
     # polarization . momentum is zero
     assert numpy.all(numpy.einsum("rim,mn,rin->ri", lmoms, η(D), lpols) == 0)
     # polarization . current is zero
-    assert numpy.all(numpy.einsum(
-        "rm,rm->r", lpols[:, 0],
-        J_μ(lmoms[:, 1:], lpols[:, 1:], put_propagator=False, verbose=True, )
-    ) == numpy.array([oPs("(32[12]^4)/([12][23][34][45][56][61])") for oPs in lPs]))
+    assert numpy.all(
+        numpy.einsum(
+            "rm,rm->r",
+            lpols[:, 0],
+            J_μ(lmoms[:, 1:], lpols[:, 1:], put_propagator=False, verbose=True),
+        )
+        == numpy.array([oPs("(32[12]^4)/([12][23][34][45][56][61])") for oPs in lPs])
+    )
